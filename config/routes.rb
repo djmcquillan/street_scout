1f51a203629968d002666219761762ba14ebee96
Dan McQuillan <djmcquillan76@gmail.com>
2015-11-02 13:37:48 -0800

    added sign-up form and functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
 
   get 'videos/:id' => 'videos#show' 
 
-  get 'users/' => 'users#index'
+  get 'signup' => 'users#new'
 
-  get 'users/show'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
