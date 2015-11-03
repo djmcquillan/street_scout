035703ad6217cc1b9e1ec03d5917dd379d32d67b
Dan McQuillan <djmcquillan76@gmail.com>
2015-11-03 14:01:55 -0800

    delete video added

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -42,7 +42,7 @@ class VideosController < ApplicationController
 
   def destroy
       Video.find(params[:id]).destroy
-      redirect_to '#'
+      redirect_to current_user
   end
     # def date_published
     #   created_at.localtime.strftime("%A, %B, %-d, %Y at %l:%M %p")
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,7 @@ module SessionsHelper
 	 def current_user
    		 @current_user ||= User.find(session[:user_id]) if session[:user_id]
    		 # redirect_to(root_url) unless current_user?(@user)
-  	end
+   end
 
 
   	 def logged_in?
